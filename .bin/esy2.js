ca1133de9fe76a6e56098c10ab7092e4fb843d75
Sander Spies <sandermail@gmail.com>
2016-11-12 17:19:01 +0100

    - Support multiple scopes per exportedEnvVar - Global is now traversing upwards the tree

--- .bin/esy2.js
+++ .bin/esy2.js
@@ -2,21 +2,20 @@
 
 
 /* TODO: type node = {
-  name: string,
-  buildCommand: string,
-  buildTimeOnlyDependencies: [node],
-  dependencies: [node],
-  envVars:  { [key: string]: string },
-  version: string,
-  target: string
-};*/
+ name: string,
+ buildCommand: string,
+ buildTimeOnlyDependencies: [node],
+ dependencies: [node],
+ envVars:  { [key: string]: string },
+ version: string,
+ target: string
+ };*/
 
 const fs = require('fs');
 const path = require('path');
 var packageResolve = require('resolve');
 
 let visitedDependencies = {};
-let globalVars = [];
 
 // get package.json of current folder or climbs up to closest one
 function findRootPackageJSON(cf) {
@@ -29,15 +28,15 @@ function findRootPackageJSON(cf) {
       }
       else {
         const newCurrentFolder = path.resolve(currentFolder, '..');
-        if (newCurrentFolder === '/') {
-          reject('couldn\'t find a package.json in the current folder or any of the parent folders');
-        }
-        else {
-          currentFolder = newCurrentFolder
-          resolver(resolve, reject);
-        }
-      }
-    });
+    if (newCurrentFolder === '/') {
+      reject('couldn\'t find a package.json in the current folder or any of the parent folders');
+    }
+    else {
+      currentFolder = newCurrentFolder
+      resolver(resolve, reject);
+    }
+  }
+  });
   }
   return new Promise(resolver);
 }
@@ -49,48 +48,47 @@ function resolvePackageJSON(pathName) {
         reject('Could not find ' + pathName);
       }
       else {
-
         let pack;
-        try {
-          pack = JSON.parse(data);
-        } catch (e) {
-          reject('Invalid JSON found at ' + pathName);
-          return;
-        }
-        if(!pack.name) {
-          reject('no package name declared in: ' + pathName);
-        }
-        const packEnvVars = pack.exportedEnvVars;
-        const esyPackage = {
-          name: pack.name,
-          build: pack.build,
-          version: pack.version,
-          exportedEnvVars: packEnvVars ? Object.keys(packEnvVars).map(
-            key => ({key:key, scope: packEnvVars[key].scope, value: packEnvVars[key].val})
-          ) : [],
-          runtimeEnvVars: null,
-          dependencies: pack.dependencies,
-          deps: []
-        };
-        const exportedEnvVars = esyPackage.exportedEnvVars;
-        esyPackage.runtimeEnvVars = exportedEnvVars.filter(({scope}) => scope === 'local');
-        globalVars = globalVars.concat(exportedEnvVars.filter(({scope}) => scope === 'global'));
-        if (esyPackage.dependencies) {
-          resolveDependencies({pathName, package: esyPackage})
-          .then(({pathName, package}) => {
-            const exportedEnvVars = package.deps.map(({package}) => package.exportedEnvVars)
-                                                .reduce((a, b) => a.concat(b))
-                                                .filter(({scope}) => scope === 'export');
-            package.runtimeEnvVars = package.runtimeEnvVars.concat(exportedEnvVars);
-            return {pathName, package};
-          })
-          .then(resolve, reject);
-        }
-        else {
-          resolve({pathName, package: esyPackage});
-        }
-      }
-    });
+    try {
+      pack = JSON.parse(data);
+    } catch (e) {
+      reject('Invalid JSON found at ' + pathName);
+      return;
+    }
+    if(!pack.name) {
+      reject('no package name declared in: ' + pathName);
+    }
+    const packEnvVars = pack.exportedEnvVars;
+    const esyPackage = {
+      name: pack.name,
+      build: pack.build,
+      version: pack.version,
+      exportedEnvVars: packEnvVars ? Object.keys(packEnvVars).map(
+        key => ({key:key, scope: packEnvVars[key].scope.split('|'), value: packEnvVars[key].val})
+  ) : [],
+      runtimeEnvVars: null,
+      dependencies: pack.dependencies,
+      deps: []
+  };
+    const exportedEnvVars = esyPackage.exportedEnvVars;
+    esyPackage.runtimeEnvVars = exportedEnvVars.filter(({scope}) => scope.includes('local'));
+    if (esyPackage.dependencies) {
+      resolveDependencies({pathName, package: esyPackage})
+        .then(({pathName, package, globalEnvVars}) => {
+        const envVars = package.deps.map(pack => pack.exportedEnvVars)
+    .reduce((a, b) => a.concat(b));
+      const exportedEnvVars = envVars.filter(envVar => envVar.scope.includes('export'));
+      const globalEnvVars2 = globalEnvVars.concat(envVars.filter(envVar => envVar.scope.includes('global')));
+      package.runtimeEnvVars = package.runtimeEnvVars.concat(exportedEnvVars).concat(globalEnvVars2);
+      return {pathName, package, globalEnvVars: globalEnvVars2};
+    })
+    .then(resolve, reject);
+    }
+    else {
+      resolve({pathName, package: esyPackage});
+    }
+  }
+  });
   }
   return new Promise(resolver);
 }
@@ -99,25 +97,26 @@ function resolveDependencies({pathName, package}) {
   // TODO: add support for peer and maybe others?!
   const depsPromises = !package.dependencies ? [] : Object.keys(package.dependencies).map(key => {
     if (!visitedDependencies[key]) {
-      visitedDependencies[key] = true;
-      return new Promise((resolve, reject) => {
-        packageResolve(path.join(key, 'package.json'), {basedir: path.dirname(pathName)}, (err, res) => {
-          if (err) {
-            reject('Could not find dependency ' + key + ', perhaps you need to run `npm install`?')
-          }
-          else {
-            resolvePackageJSON(res).then(resolve, reject);
-          }
-        });
-      });
-    }
+    visitedDependencies[key] = true;
+    return new Promise((resolve, reject) => {
+      packageResolve(path.join(key, 'package.json'), {basedir: path.dirname(pathName)}, (err, res) => {
+      if (err) {
+        reject('Could not find dependency ' + key + ', perhaps you need to run `npm install`?')
+      }
+      else {
+        resolvePackageJSON(res).then(resolve, reject);
+      }
+    });
   });
+  }
+});
   return Promise.all(depsPromises)
     .then(resolvedDeps => {
-      package.dependencies = null;
-      package.deps = resolvedDeps;
-      return {pathName, package};
-    });
+    package.dependencies = null;
+  package.deps = resolvedDeps.map(x => x.package);
+  const globalEnvVars = resolvedDeps.map(dep => dep.globalEnvVars || []).reduce((envVarA, envVarB) => envVarA.concat(envVarB));
+  return {pathName, package, globalEnvVars};
+});
 }
 
 const util = require('util');
@@ -126,6 +125,6 @@ function printCurrentSandBox({pathName, package}) {
 }
 
 findRootPackageJSON(process.cwd())
-.then(resolvePackageJSON)
-.then(printCurrentSandBox)
-.catch(err => console.info('error:', err));
+  .then(resolvePackageJSON)
+  .then(printCurrentSandBox)
+  .catch(err => console.info('error:', err));
