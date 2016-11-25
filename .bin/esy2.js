5d6c7790d24d4531d85570e712e773def8ab6249
Sander Spies <sandermail@gmail.com>
2016-11-25 12:18:10 +0100

    Use export instead

--- .bin/esy2.js
+++ .bin/esy2.js
@@ -1,24 +1,27 @@
+#!/usr/bin/env node
+
 /* @flow */
 const fs = require('fs');
 const path = require('path');
 const packageResolve = require('resolve');
 
 type scope =
-| "export"
+|
+"export"
 | "global"
 | "local";
 
 type envVar = {
   key: string,
   val: string,
-  scope: string
+  scope?: string
 };
 
 type npmModule = {
   build: string,
   dependencies: { [key: string]: string },
-exportedEnvVars: { [key: string]: envVar },
-name: string,
+  exportedEnvVars: { [key: string]: envVar },
+  name: string,
   version: string
 };
 
@@ -42,12 +45,13 @@ type _visitedDependencies = {
   [key: string]: boolean
 };
 
-let visitedDependencies:_visitedDependencies = {};
+let visitedDependencies: _visitedDependencies = {};
 
 // get package.json of current folder or climbs up to closest one
 function findRootPackageJSON(cf) {
   let currentFolder = cf;
-  function resolver (resolve, reject) {
+
+  function resolver(resolve, reject) {
     const packageJSONpath = path.join(currentFolder, 'package.json');
     fs.exists(packageJSONpath, exists => {
       if (exists) {
@@ -55,16 +59,17 @@ function findRootPackageJSON(cf) {
       }
       else {
         const newCurrentFolder = path.resolve(currentFolder, '..');
-    if (newCurrentFolder === '/') {
-      reject('couldn\'t find a package.json in the current folder or any of the parent folders');
-    }
-    else {
-      currentFolder = newCurrentFolder
-      resolver(resolve, reject);
-    }
+        if (newCurrentFolder === '/') {
+          reject('couldn\'t find a package.json in the current folder or any of the parent folders');
+        }
+        else {
+          currentFolder = newCurrentFolder
+          resolver(resolve, reject);
+        }
+      }
+    });
   }
-  });
-  }
+
   return new Promise(resolver);
 }
 
@@ -76,47 +81,51 @@ function resolvePackageJSON(pathName) {
       }
       else {
         try {
-          var pack:npmModule = JSON.parse(data);
-  } catch (e) {
-      reject('Invalid JSON found at ' + pathName);
-      return;
-    }
-    if(!pack.name) {
-      reject('no package name declared in: ' + pathName);
-    }
-    const packEnvVars = pack.exportedEnvVars;
-    const esyPackage:esyPackage = {
-      name: pack.name,
-      build: pack.build,
-      version: pack.version,
-      exportedEnvVars: packEnvVars ? Object.keys(packEnvVars).map(
-        key => ({key:key, scope: packEnvVars[key].scope.split('|'), value: packEnvVars[key].val})
-  ) : [],
-      runtimeEnvVars: [],
-      dependencies: pack.dependencies,
-      deps: []
-  };
-    const exportedEnvVars = esyPackage.exportedEnvVars;
+          var pack: npmModule = JSON.parse(data);
+        }
+        catch (e) {
+          reject('Invalid JSON found at ' + pathName);
+          return;
+        }
+        if (!pack.name) {
+          reject('no package name declared in: ' + pathName);
+        }
+        const packEnvVars = pack.exportedEnvVars;
+        const esyPackage: esyPackage = {
+          name: pack.name,
+          build: pack.build,
+          version: pack.version,
+          exportedEnvVars: packEnvVars ? Object.keys(packEnvVars).map(
+            key => ({key: key, scope: packEnvVars[key].scope ? packEnvVars[key].scope.split('|') : [], value: packEnvVars[key].val})
+          ) : [],
+          runtimeEnvVars: [],
+          dependencies: pack.dependencies,
+          deps: []
+        };
+        const exportedEnvVars = esyPackage.exportedEnvVars;
 
-    esyPackage.runtimeEnvVars = esyPackage.runtimeEnvVars.concat(exportedEnvVars.filter(({scope}) => scope.includes('local')));
-    if (esyPackage.dependencies) {
-      resolveDependencies({pathName, module: esyPackage})
-        .then(({pathName, module, globalEnvVars}) => {
-        const envVars:[esyEnvVar] = module.deps.map(pack => pack.exportedEnvVars)
-    .reduce((a, b) => a.concat(b));
-      const exportedEnvVars = envVars.filter(envVar => envVar.scope.includes('export'));
-      const globalEnvVars2 = globalEnvVars.concat(envVars.filter(envVar => envVar.scope.includes('global')));
-      module.runtimeEnvVars = module.runtimeEnvVars.concat(exportedEnvVars).concat(globalEnvVars2);
-      return {pathName, module, globalEnvVars: globalEnvVars2};
-    })
-    .then(resolve, reject);
-    }
-    else {
-      resolve({pathName, module: esyPackage});
-    }
+        esyPackage.runtimeEnvVars = esyPackage.runtimeEnvVars.concat(exportedEnvVars.filter(({scope}) => scope.includes('local')));
+        if (esyPackage.dependencies) {
+          resolveDependencies({pathName, module: esyPackage})
+            .then(({pathName, module, globalEnvVars}) => {
+              let envVars = module.deps.map(pack => pack.exportedEnvVars);
+              if (envVars.length) {
+                envVars = envVars.reduce((a, b) => a.concat(b));
+              }
+              const exportedEnvVars = envVars.filter(envVar => envVar.scope.includes('export'));
+              const globalEnvVars2 = globalEnvVars.concat(envVars.filter(envVar => envVar.scope.includes('global')));
+              module.runtimeEnvVars = module.runtimeEnvVars.concat(exportedEnvVars).concat(globalEnvVars2);
+              return {pathName, module, globalEnvVars: globalEnvVars2};
+            })
+            .then(resolve, reject);
+        }
+        else {
+          resolve({pathName, module: esyPackage});
+        }
+      }
+    });
   }
-  });
-  }
+
   return new Promise(resolver);
 }
 
@@ -124,25 +133,31 @@ function resolveDependencies({pathName, module}) {
   // TODO: add support for peer and maybe others?!
   const depsPromises = !module.dependencies ? [] : Object.keys(module.dependencies).map(key => {
     if (!visitedDependencies[key]) {
-    visitedDependencies[key] = true;
-    return new Promise((resolve, reject) => {
-      packageResolve(path.join(key, 'package.json'), {basedir: path.dirname(pathName)}, (err, res) => {
-      if (err) {
-        reject('Could not find dependency ' + key + ', perhaps you need to run `npm install`?')
+      visitedDependencies[key] = true;
+      return new Promise((resolve, reject) => {
+        packageResolve(path.join(key, 'package.json'), {basedir: path.dirname(pathName)}, (err, res) => {
+          if (err) {
+            reject('Could not find dependency ' + key + ', perhaps you need to run `npm install`?')
+          }
+          else {
+            resolvePackageJSON(res).then(resolve, reject);
+          }
+        });
+      });
+    }
+  });
+  return Promise.all(depsPromises)
+    .then((resolvedDeps: [{module: npmModule, globalEnvVars:[esyEnvVar]}]) => {
+      module.deps = resolvedDeps.map(dependency => dependency.module);
+      let globalEnvVars = resolvedDeps.map(dep => dep.globalEnvVars || []);
+      if (globalEnvVars.length) {
+        globalEnvVars = globalEnvVars.reduce((envVarA, envVarB) => envVarA.concat(envVarB));
       }
       else {
-        resolvePackageJSON(res).then(resolve, reject);
+        globalEnvVars = [];
       }
+      return {pathName, module, globalEnvVars};
     });
-  });
-  }
-});
-  return Promise.all(depsPromises)
-    .then((resolvedDeps:[{module: npmModule, globalEnvVars:[esyEnvVar]}]) => {
-    module.deps = resolvedDeps.map(dependency => dependency.module);
-    const globalEnvVars:[esyEnvVar] = resolvedDeps.map(dep => dep.globalEnvVars || []).reduce((envVarA, envVarB) => envVarA.concat(envVarB));
-    return {pathName, module, globalEnvVars};
-  });
 }
 
 const util = require('util');
@@ -150,14 +165,14 @@ function printCurrentSandBox({pathName, module}) {
   console.log(util.inspect(module, {showHidden: false, depth: null}))
 }
 
-const processedBuildTasks:_visitedDependencies = {};
+const processedBuildTasks: _visitedDependencies = {};
 function createBuildTask({pathName, module}) {
   if (!processedBuildTasks[module.name]) {
     processedBuildTasks[module.name] = true;
     const childBuildTasks = module.deps.map(m => createBuildTask({pathName: null, module: m})).join('');
     const buildDeps = module.deps.map(m => m.name);
     let buildTask = childBuildTasks + (buildDeps.length ? module.name + ' : ' + buildDeps.join(' ') : module.name) + '\n';
-    buildTask += '\t' + module.runtimeEnvVars.map(envVar => envVar.key + '=' + envVar.value).join(' ') + ' ' + module.build + '\n\n';
+    buildTask += module.runtimeEnvVars.map(envVar => '\texport ' + envVar.key + '=' + envVar.value).join('\n') + '\n\t' + module.build + '\n\n';
     if (pathName) {
       buildTask += 'default: ' + module.name;
     }
@@ -172,4 +187,4 @@ findRootPackageJSON(process.cwd())
   .then(resolvePackageJSON)
   .then(createBuildTask)
   .then(a => console.info(a))
-.catch((err:string) => console.info('error:', err));
+  .catch((err: string) => console.info('error:', err));
