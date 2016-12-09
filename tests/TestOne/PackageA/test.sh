1633866802dfcef3abb5dedc495d4dda4a9fc12e
Andrey Popp <8mayday@gmail.com>
2016-12-09 11:17:12 +0300

    Move ocaml to peerDependencies

--- tests/TestOne/PackageA/test.sh
+++ tests/TestOne/PackageA/test.sh
@@ -1,3 +1,5 @@
+npm install andreypopp/ocaml#trunk
+
 cd ../
 pushd PackageC && rm -rf node_modules && popd && \
 pushd PackageB && rm -rf node_modules && popd && \
@@ -9,5 +11,3 @@ cd ../PackageC && npm link buildtool && npm install && npm link && \
 cd ../PackageB && npm link buildtool && npm link PackageC && npm install && npm link && \
 cd ../buildtool && npm install && npm link && \
 cd ../PackageA && npm link PackageC && npm link PackageB && npm link buildtool && npm install
-
-../../../.bin/esy $1
