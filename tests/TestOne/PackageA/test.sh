b520a1199d76121658ff744c38cf005b0ddaace2
Andrey Popp <8mayday@gmail.com>
2016-12-12 16:27:31 +0300

    Simplify test packages, make them not to use solvuu-build

--- tests/TestOne/PackageA/test.sh
+++ tests/TestOne/PackageA/test.sh
@@ -11,3 +11,5 @@ cd ../PackageC && npm link buildtool && npm install && npm link && \
 cd ../PackageB && npm link buildtool && npm link PackageC && npm install && npm link && \
 cd ../buildtool && npm install && npm link && \
 cd ../PackageA && npm link PackageC && npm link PackageB && npm link buildtool && npm install
+
+../../../.bin/esy build
