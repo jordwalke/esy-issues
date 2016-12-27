e89de8e1cb34daba74f4fe13bd7ab7485bebb1b5
Andrey Popp <8mayday@gmail.com>
2016-12-27 15:16:11 +0300

    Remove remote cache mechanism for now, simplify eject code

--- lib/esyBuildRuntime.sh
+++ lib/esyBuildRuntime.sh
@@ -2,9 +2,6 @@ set -e
 set -u
 set -o pipefail
 
-ESY__BUILD_CACHE_URL_PREFIX="https://github.com/andreypopp/esy/releases/download/build-cache"
-ESY__BUILD_CACHE_URL="$ESY__BUILD_CACHE_URL_PREFIX/$cur__install_key.tar.gz"
-
 ESY__BUILD_COMMAND="
 let esy = require(\"$cur__root/package.json\").esy || {};\
 let build = esy.build || 'true';\
@@ -27,14 +24,6 @@ esy-shell () {
   /bin/bash \
     --noprofile \
     --rcfile <(echo "export PS1=\"[$cur__name sandbox] $ \"; source $ESY__RUNTIME")
-}
-
-esy-build-archive () {
-  # TODO: we depend on GNU tar here, make sure we get rid of that
-  gtar \
-    -czf $cur__install_key.tar.gz \
-    --transform "s,^\.,$cur__install_key," \
-    -C $cur__install .
 }
 
 esy-build-command () {
@@ -61,44 +50,13 @@ esy-clean () {
 }
 
 esy-build () {
+  # TODO: that's a fragile check, we need to build in another location and then
+  # mv to the $cur__install. Why we don't do this now is because we don't
+  # assume everything we build is relocatable.
   if [ ! -d "$cur__install" ]; then
-    echo -n "*** $cur__name: checking if cached build artifact is available... "
-    cache_build_status=`esy-build-download-status`
-    if [ "$cache_build_status" != "404" ]; then
-      echo "found, downloading... "
-      esy-build-download
-    else
-      echo "not found, building from sources..."
-      esy-prepare-install-tree
-      esy-build-command
-    fi
+    echo "*** $cur__name: building from source... "
+    esy-prepare-install-tree
+    # TODO: we need proper locking mechanism here
+    esy-build-command
   fi
-}
-
-esy-build-fetch () {
-  if [ ! -d "$cur__install" ]; then
-    set +e
-    esy-build-download
-    set -e
-  fi
-}
-
-esy-build-download-status () {
-  curl                            \
-    -s                            \
-    -o /dev/null                  \
-    -w "%{http_code}"             \
-    -I "$ESY__BUILD_CACHE_URL"
-}
-
-esy-build-download () {
-  cd $esy__store/_install
-  wget                              \
-    --continue                      \
-    --tries 10                      \
-    --timestamping                  \
-    --show-progress                 \
-    --quiet                         \
-    "$ESY__BUILD_CACHE_URL" &&      \
-  tar -xzf $cur__install_key.tar.gz
 }
